0aec48c02e046ce7a2d4174017170ca922c54cf9
ReDuzed <46213546+ReDuzed@users.noreply.github.com>
2019-05-26 19:20:02 -0700

    v0.3.9+
    -Enabled background

--- Backgrounds/bg_style.cs
+++ Backgrounds/bg_style.cs
@@ -12,11 +12,10 @@ namespace ArchaeaMod.Backgrounds
 
         public override void FillTextureArray(int[] textureSlots)
         {
-            /*textureSlots[0] = mod.GetBackgroundSlot("Backgrounds/bg_magno");
+            textureSlots[0] = mod.GetBackgroundSlot("Backgrounds/bg_magno");
             textureSlots[1] = mod.GetBackgroundSlot("Backgrounds/bg_magno_surface");
             textureSlots[2] = mod.GetBackgroundSlot("Backgrounds/bg_magno_connector");
             textureSlots[3] = mod.GetBackgroundSlot("Backgrounds/bg_magno");
-            */
         }
     }   
 } 
