79faf38b7983adf2cf1fc10ca5ab78184e0aec77
ReDuzed <46213546+ReDuzed@users.noreply.github.com>
2019-05-31 16:20:07 -0700

    v0.4.2.10
    -Large crystal with placeholder sprite

---
+++ Tiles/c_crystal_large.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.Enums;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace ArchaeaMod.Tiles
+{
+    public class c_crystal_large : ModTile
+    {
+        public override void SetDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileLighted[Type] = true;
+            Main.tileNoAttach[Type] = true;
+            TileID.Sets.NotReallySolid[Type] = true;
+            TileID.Sets.DrawsWalls[Type] = true;
+            TileObjectData.newTile.Width = 2;
+            TileObjectData.newTile.Height = 3;
+            TileObjectData.newTile.Origin = new Point16(0, 0);
+            TileObjectData.newTile.CoordinateWidth = 16;
+            TileObjectData.newTile.CoordinateHeights = new int[] { 16, 16, 16 };
+            TileObjectData.newTile.CoordinatePadding = 2;
+            TileObjectData.newTile.AnchorInvalidTiles = new int[] { 127 };
+            TileObjectData.newTile.StyleHorizontal = true;
+            TileObjectData.newTile.AnchorBottom = new AnchorData(AnchorType.SolidTile, TileObjectData.newTile.Width, 0);
+            TileObjectData.newAlternate.CopyFrom(TileObjectData.newTile);
+            TileObjectData.newAlternate.Origin = new Point16(0, 1);
+            TileObjectData.addAlternate(0);
+            TileObjectData.addTile(Type);
+            ModTranslation name = CreateMapEntryName();
+            name.SetDefault("Cinnabar Crystal");
+            AddMapEntry(new Color(210, 110, 110), name);
+            disableSmartCursor = true;
+            soundStyle = 27;
+            soundType = 2;
+            mineResist = 2f;
+            minPick = 45;
+        }
+        public override void ModifyLight(int i, int j, ref float r, ref float g, ref float b)
+        {
+            r = 0.604f;
+            g = 0.161f;
+            b = 0.161f;
+        }
+
+        public override void KillTile(int i, int j, ref bool fail, ref bool effectOnly, ref bool noItem)
+        {
+            noItem = true;
+        }
+        public override void KillMultiTile(int i, int j, int frameX, int frameY)
+        {
+            int random = Main.rand.Next(4, 8);
+            for (int k = 0; k < random; k++)
+            {
+                Item.NewItem(new Vector2(i * 16, j * 16), mod.ItemType("cinnabar_crystal"), random, true, 0, true, false);
+            }
+        }
+        public override bool CanKillTile(int i, int j, ref bool blockDamaged)
+        {
+            return mod.GetModWorld<ArchaeaWorld>().downedMagno;
+        }
+        public override bool CanExplode(int i, int j)
+        {
+            return mod.GetModWorld<ArchaeaWorld>().downedMagno;
+        }
+
+        Texture2D texture;
+        public override bool PreDraw(int i, int j, SpriteBatch spriteBatch)
+        {
+            SpriteEffects effects = SpriteEffects.None;
+            Tile tile = Main.tile[i, j];
+
+            texture = Main.tileTexture[Type];
+
+            Vector2 zero = new Vector2(Main.offScreenRange, Main.offScreenRange);
+            if (Main.drawToScreen)
+            {
+                zero = Vector2.Zero;
+            }
+
+            Main.spriteBatch.Draw(texture,
+                new Vector2(i * 16 - (int)Main.screenPosition.X, j * 16 - (int)Main.screenPosition.Y) + zero,
+                new Rectangle(tile.frameX, tile.frameY, 16, 16),
+                Lighting.GetColor(i, j), 0f, default(Vector2), 1f, effects, 0f);
+
+            return false;
+        }
+    }
+}
