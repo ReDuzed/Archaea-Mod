3ad585248fd8266dc27fe603a27429b470e40dfa
ReDuzed <46213546+ReDuzed@users.noreply.github.com>
2019-05-31 15:24:04 -0700

    v0.4.2

--- Tiles/ArchaeaTiles.cs
+++ Tiles/ArchaeaTiles.cs
@@ -40,5 +40,50 @@ namespace ArchaeaMod.Tiles
                 }
             return base.CanKillTile(i, j, type, ref blockDamaged);
         }
+        private int counter;
+        public override void RandomUpdate(int i, int j, int type)
+        {
+            if (!ArchaeaWorld.Inbounds(i, j))
+                return;
+            if (Main.rand.NextFloat() > 0.95f && type == ArchaeaWorld.magnoStone)
+            {
+                int count = 0;
+                ushort[] types = new ushort[]
+                {
+                    ArchaeaWorld.crystal,
+                    ArchaeaWorld.crystal2x1,
+                    ArchaeaWorld.crystal2x2
+                };
+                for (int k = i - 8; k < i + 8; k++)
+                for (int l = j - 8; l < j + 8; l++)
+                {
+                    foreach (ushort t in types)
+                    {
+                        if (Main.tile[k, l].type == t)
+                        {
+                            count++;
+                        }
+                    }
+                }
+                if (count == 0)
+                {
+                    Tile top = Main.tile[i, j + 1];
+                    Tile right = Main.tile[i + 1, j];
+                    Tile bottom = Main.tile[i, j - 1];
+                    Tile left = Main.tile[i - 1, j];
+                    if (type == ArchaeaWorld.magnoStone)
+                    {
+                        if (!top.active())
+                            WorldGen.PlaceTile(i, j - 1, (int)types[0], true, false, -1, 3);
+                        else if (!right.active())
+                            WorldGen.PlaceTile(i, j - 1, (int)types[0], true, false, -1, 1);
+                        else if (!bottom.active())
+                            WorldGen.PlaceTile(i, j - 1, Main.rand.Next(new int[] { (int)types[0], (int)types[1], (int)types[3] }), true, false, -1, 0);
+                        else if (!left.active())
+                            WorldGen.PlaceTile(i, j - 1, (int)types[0], true, false, -1, 2);
+                    }
+                }
+            }
+        }
     }
 }
