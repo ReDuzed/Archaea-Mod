869d9f9f89127fa93dc6cd59281e26339d34f424
ReDuzed <46213546+ReDuzed@users.noreply.github.com>
2019-06-26 13:32:42 -0700

    v0.4.3

---
+++ Tiles/sky_portal.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace ArchaeaMod.Tiles
+{
+    public class sky_portal : ModTile
+    {
+        public override void SetDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            TileID.Sets.NotReallySolid[Type] = true;
+            TileID.Sets.DrawsWalls[Type] = true;
+            TileObjectData.newTile.Width = 3;
+            TileObjectData.newTile.Height = 3;
+            TileObjectData.newTile.UsesCustomCanPlace = true;
+            TileObjectData.newTile.CoordinateWidth = 16;
+            TileObjectData.newTile.CoordinateHeights = new int[] { 16, 16, 16 };
+            TileObjectData.newTile.CoordinatePadding = 2;
+            TileObjectData.newTile.AnchorWall = true;
+            TileObjectData.newTile.Origin = new Point16(1, 1);
+            TileObjectData.addTile(Type);
+            ModTranslation name = CreateMapEntryName();
+            name.SetDefault("Portal");
+            AddMapEntry(new Color(0.6f, 0.6f, 0f), name);
+        }
+
+        public override void NumDust(int i, int j, bool fail, ref int num)
+        {
+            fail = true;
+            num = 0;
+        }
+        public override void KillTile(int i, int j, ref bool fail, ref bool effectOnly, ref bool noItem)
+        {
+            fail = true;
+            effectOnly = true;
+            noItem = true;
+        }
+        public override bool CanKillTile(int i, int j, ref bool blockDamaged)
+        {
+            return false;
+        }
+        public override bool CanExplode(int i, int j)
+        {
+            return false;
+        }
+    }
+}
